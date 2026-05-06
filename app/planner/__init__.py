2fbfd6b1ea275f3dbdf6b95ee02eca46ed9edc37
Killian Baillifard <killian.baillifard@gmail.com>
2026-05-06 17:22:52 +0200

    Added planner test

--- app/planner/__init__.py
+++ app/planner/__init__.py
@@ -5,8 +5,24 @@ from app.inputs.setpoint import Setpoint
 
 class Planner:
 
-    def __init__(self) -> None:
-        pass
+    TOLERANCE = 0.01
 
-    def update(measurement: Measurement, frame: MatLike) -> Setpoint:
-        return Setpoint(glm.vec3(0.0, 0.0, 0.0), 0.0)
+    def __init__(self) -> None:
+        self.waypoints = [
+            glm.vec3(0.0, 0.0, 1.0),
+            glm.vec3(-1.0, 0.0, 1.0),
+            glm.vec3(-1.0, -0.5, 1.0),
+            glm.vec3(-1.0, 0.5, 1.0),
+            glm.vec3(-1.0, 0.0, 1.0),
+            glm.vec3(-1.0, 0.0, 0.2)
+        ]
+        self.i = 0
+
+    def update(self, measurement: Measurement, frame: MatLike) -> Setpoint:
+        if self.i < len(self.waypoints):
+            setpoint = Setpoint(self.waypoints[self.i], 0.0)
+            if glm.distance(self.waypoints[self.i], measurement.position) < Planner.TOLERANCE:
+                self.i += 1
+            return setpoint
+        else:
+            return Setpoint(self.waypoints[-1], 0.0)
