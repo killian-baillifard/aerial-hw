9162b387ec87c79a69d243ba581ab634713dbd21
Killian Baillifard <killian.baillifard@gmail.com>
2026-05-09 17:22:06 +0200

    Added ground sector lines

--- app/gui/widgets/scene.py
+++ app/gui/widgets/scene.py
@@ -1,3 +1,4 @@
+import numpy as np
 from pyglm import glm
 from pygame import Surface, draw
 from overrides import override
@@ -13,8 +14,13 @@ class Scene(Widget):
 
     def __init__(self, position: glm.uvec2, size: glm.uvec2, z_index: int = 0) -> None:
         super().__init__(z_index)
-        self.offset = position
-        self.box = Mesh(
+
+        # Compute scale factor that maximizes fill while preserving aspect ratio
+        self.scale = min(size.x / CAM_W, size.y / CAM_H)
+        self.offset = position + glm.uvec2(int((size.x - self.scale * CAM_W) / 2), 0.0)
+
+        # Generate room mesh
+        self.room = Mesh(
             [
                 glm.vec3(-Scene.DEPTH / 2, -Scene.WIDTH / 2, 0.0),
                 glm.vec3(-Scene.DEPTH / 2, -Scene.WIDTH / 2, Scene.HEIGHT),
@@ -26,29 +32,34 @@ class Scene(Widget):
                 glm.vec3(+Scene.DEPTH / 2, +Scene.WIDTH / 2, Scene.HEIGHT),
             ],
             [
-                # Back face (-D)
                 (0, 1), (1, 3), (3, 2), (2, 0),
-
-                # Front face (+D)
                 (4, 5), (5, 7), (7, 6), (6, 4),
-
-                # Connecting edges
                 (0, 4), (1, 5), (2, 6), (3, 7)
             ]
         )
-        self.box_lines: list[Line] = []
 
-        # Compute scale factor that maximizes fill while preserving aspect ratio
-        self.scale = min(size.x / CAM_W, size.y / CAM_H)
-        self.offset += glm.uvec2(int((size.x - self.scale * CAM_W) / 2), 0.0)
+        # Generate sectors mesh
+        sectors_vertices = [glm.vec3(0.0, 0.0, 0.0)]
+        sectors_indices = []
+        for i in range(10):
+            angle = np.deg2rad(45) + i * np.deg2rad(30)
+            x = -(Scene.WIDTH / 2) * np.cos(angle)
+            y = -(Scene.WIDTH / 2) * np.sin(angle)
+            sectors_vertices.append(glm.vec3(x, y, 0.0))
+            sectors_indices.append((0, i + 1))
+        self.sectors = Mesh(sectors_vertices, sectors_indices)
+
+        # Create empty lines to draw
+        self.lines_to_draw: list[Line] = []
 
     def set_view(self, position: glm.vec3, rotation: glm.vec3):
         view_matrix = view(position, euler_to_quaternion(rotation))
-        self.box_lines = self.box.project(view_matrix)
+        self.lines_to_draw.clear()
+        self.lines_to_draw = self.room.project(view_matrix) + self.sectors.project(view_matrix)
 
     @override
     def draw(self, surface: Surface) -> None:
-        for line in self.box_lines:
+        for line in self.lines_to_draw:
             scaled_begin = self.scale * line.begin
             scaled_end = self.scale * line.end
             offset_begin = self.offset + glm.uvec2(int(scaled_begin.x), int(scaled_begin.y))
